78addc7b8c86ece59251cdcbad868f6387c95091
Marco Orban <marco.orban@gmail.com>
2025-02-21 18:25:10 +0800

    added sensor and readings models

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from server import db
@@ -10,3 +11,27 @@ class Room(db.Model):
     
     def __repr__(self):
         return '<Room {}>'.format(self.name)
+    
+
+class Sensor(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                            unique=True)
+    room: so.Mapped[Room] = so.mapped_column(sa.ForeignKey(Room.id),
+                                             index=True)
+    readings: so.WriteOnlyMapped['Reading'] = so.relationship(back_populates='sensed_by')
+    
+    def __repr__(self):
+        return '<Sensor {} @ {}>'.format(self.name, self.room)
+    
+class Reading(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    temperature: so.Mapped[float] = so.mapped_column()
+    humidity: so.Mapped[float] = so.mapped_column()
+    pressure: so.Mapped[Optional[float]] = so.mapped_column()
+    heat_index: so.Mapped[float] = so.mapped_column()
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True)
+    sensor: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Sensor.id),
+                                              index=True)
+    sensed_by: so.Mapped[Sensor] = so.relationship(back_populates='readings')
+    
