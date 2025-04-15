cbe04fa1faf7acc2b33bc297377c0f472f8cc578
Marco Orban <marco.orban@gmail.com>
2025-04-15 10:54:07 +0800

    Added sensor list to import csv form

--- datacollect/forms.py
+++ datacollect/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from .models import Sensor
 
+sensors = Sensor.objects.all()
+sensor_choices = {sensor.name: sensor.name for sensor in sensors}
 
 class ImportCSVForm(forms.Form):
-    csvfile = forms.FileField(label="csv-file")
+    csvfile = forms.FileField(label="Csv file")
+    sensor = forms.ChoiceField(choices=(sensor_choices), label="Sensor")
